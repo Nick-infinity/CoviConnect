0736b557e92d400543221a45818814e50f271104
Nickinfinity <nikhilg.me@gmail.com>
2021-04-26 13:07:17 +0530

    AuthContext: setup dummy functions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,48 @@ const authReducer = (state, action) => {
 	}
 };
 
+
+//signup 
+const signup = (dispatch) ={
+    return ({email,password})=>{
+        // make api request to sigup with email and password
+
+        //if we are signup, modify our state and say we are authenticatedd
+
+
+        // if sigunup fails , reflect error mesg
+
+
+    };
+};
+
+//signin 
+const signin = (dispatch) ={
+    return ({email,password})=>{ 
+        // make api request to sigin with email and password
+
+        //if we are signin  modify our state and say we are authenticatedd
+
+
+        // if siguin fails , reflect error mesg
+
+
+    };
+};
+
+//signout
+const signout = (dispatch) ={
+    return ()=>{
+        //signout
+
+
+    };
+};
+
+
+
 export const { Provider, Context } = createDataContext(
 	authReducer,
-	{},
+	{signin,signout,signup},
 	{ isSignedIn: false }
 );
