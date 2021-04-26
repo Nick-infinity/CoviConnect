336b7d94c0059580ac183a747bc321203c3a4cfc
Nickinfinity <nikhilg.me@gmail.com>
2021-04-26 16:50:03 +0530

    AccountScreen: Add logout button

--- src/screen/AccountsScreen.js
+++ src/screen/AccountsScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { Button } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountsScreen = () => {
+	const { state, signout } = useContext(AuthContext);
 	return (
-		<View>
+		<View style={{ marginTop: 400 }}>
 			<Text>AccountsScreen</Text>
+			<Button title="Logout" onPress={() => signout()} />
 		</View>
 	);
 };
