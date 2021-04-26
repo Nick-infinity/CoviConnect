2f15dad8ea080e74331669977f192435a847c974
Nickinfinity <nikhilg.me@gmail.com>
2021-04-26 13:39:50 +0530

    Fix some typos

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: ' http://778d3359784d.ngrok.io', // this will change based on ngrok
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
 	switch (action.type) {
@@ -7,48 +8,33 @@ const authReducer = (state, action) => {
 	}
 };
 
-
-//signup 
-const signup = (dispatch) ={
-    return ({email,password})=>{
-        // make api request to sigup with email and password
-
-        //if we are signup, modify our state and say we are authenticatedd
-
-
-        // if sigunup fails , reflect error mesg
-
-
-    };
+//signup
+const signup = (dispatch) => {
+	return ({ email, password }) => {
+		// make api request to sigup with email and password
+		//if we are signup, modify our state and say we are authenticatedd
+		// if sigunup fails , reflect error mesg
+	};
 };
 
-//signin 
-const signin = (dispatch) ={
-    return ({email,password})=>{ 
-        // make api request to sigin with email and password
-
-        //if we are signin  modify our state and say we are authenticatedd
-
-
-        // if siguin fails , reflect error mesg
-
-
-    };
+//signin
+const signin = (dispatch) => {
+	return ({ email, password }) => {
+		// make api request to sigin with email and password
+		//if we are signin  modify our state and say we are authenticatedd
+		// if siguin fails , reflect error mesg
+	};
 };
 
 //signout
-const signout = (dispatch) ={
-    return ()=>{
-        //signout
-
-
-    };
+const signout = (dispatch) => {
+	return () => {
+		//signout
+	};
 };
-
-
 
 export const { Provider, Context } = createDataContext(
 	authReducer,
-	{signin,signout,signup},
-	{ isSignedIn: false }
+	{ signin, signout, signup },
+	{ userToke: '' }
 );
