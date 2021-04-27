e20460ae1edc748ec260a8fe9d3964a4d942565a
Nickinfinity <nikhilg.me@gmail.com>
2021-04-28 00:12:04 +0530

    PlasmaHospital: Add Fields and Styles

--- src/screen/forms/PlasmaHospital.js
+++ src/screen/forms/PlasmaHospital.js
@@ -1,9 +1,162 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Input, Button, Icon, ButtonGroup } from 'react-native-elements';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { Input, Button, Icon, ButtonGroup, Text } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const PlasmaHospital = () => {
-	return <View></View>;
+	// states
+	const [name, setName] = useState('');
+	const [contact, setContact] = useState('');
+	const [pin, setPin] = useState('');
+	const [consent, SetConsent] = useState(false);
+	const [availability, SetAvailability] = useState(0);
+	const availabilityOptions = ['Available', 'Not Available'];
+	const [bloodGroup, setBloodGroup] = useState();
+	const bloodGroups = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'];
+
+	//check validity of data on submit
+	const isValid = (value, setValue) => {};
+
+	//get state and city from custom api and validate pin
+	const isPinValid = () => {};
+
+	// onClick for save button
+	const onSaveClick = () => {};
+
+	return (
+		<SafeAreaView forceInset={{ top: 'always' }}>
+			<View style={styles.container}>
+				<ScrollView>
+					<View style={styles.secondContainer}>
+						<Text h2 style={styles.banner}>
+							Hospital Information
+						</Text>
+						<View style={styles.formContainer}>
+							<View style={styles.fieldContainer}>
+								<Input
+									placeholder="Enter hospital name"
+									label="Hospital Name"
+									value={name}
+									onChangeText={(t) => setName(t)}
+									inputContainerStyle={inputStyle}
+								/>
+								<Input
+									placeholder="Enter active phone number "
+									label="Plasma Helpline"
+									value={contact}
+									onChangeText={(t) => setContact(t)}
+									inputContainerStyle={inputStyle}
+								/>
+								<Input
+									keyboardType="numeric"
+									placeholder="Enter area pincode"
+									label="Pincode"
+									value={pin}
+									onChangeText={(t) => setPin(t)}
+									inputContainerStyle={inputStyle}
+								/>
+								<Text style={styles.btnGrpBannerStyle}>
+									Plasma availability
+								</Text>
+								<ButtonGroup
+									style={styles.btnGroupStyle}
+									onPress={(num) => SetAvailability(num)}
+									selectedIndex={availability}
+									buttons={availabilityOptions}
+									containerStyle={btnGroupStyle}
+								/>
+								<Text style={styles.btnGrpBannerStyle}>
+									Available bloodgroups
+								</Text>
+								<ButtonGroup
+									onPress={(text) => setBloodGroup(text)}
+									selectedIndex={bloodGroup}
+									buttons={bloodGroups}
+									containerStyle={btnGroupStyle}
+								/>
+							</View>
+						</View>
+						<TouchableOpacity style={styles.btnStyle}>
+							<View style={styles.btnContainer}>
+								<Text h4 style={styles.btnTextStyle}>
+									Save
+								</Text>
+							</View>
+						</TouchableOpacity>
+					</View>
+				</ScrollView>
+			</View>
+		</SafeAreaView>
+	);
 };
 
+const btnGroupStyle = {
+	marginBottom: 20,
+	height: 30,
+	borderRadius: 12,
+};
+const inputStyle = {
+	borderBottomWidth: 1,
+	borderWidth: 1,
+	borderColor: 'gray',
+	borderRadius: 15,
+	paddingHorizontal: 10,
+	paddingVertical: 5,
+	marginTop: 10,
+};
+
+const styles = StyleSheet.create({
+	banner: {
+		alignSelf: 'center',
+	},
+	container: {
+		marginTop: 70,
+	},
+	secondContainer: {
+		marginBottom: 40,
+	},
+	formContainer: {
+		marginHorizontal: 10,
+		borderRadius: 20,
+		backgroundColor: 'white',
+		marginTop: 30,
+	},
+	fieldContainer: {
+		marginTop: 30,
+		marginBottom: 30,
+	},
+	btnGrpBannerStyle: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		color: '#87929d',
+		marginLeft: 10,
+		marginBottom: 10,
+	},
+	btnStyle: {
+		borderRadius: 20,
+	},
+	btnContainer: {
+		borderWidth: 1,
+		borderRadius: 20,
+		borderColor: 'gray',
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+		marginHorizontal: 40,
+		alignItems: 'center',
+		marginTop: 20,
+		backgroundColor: '#272727',
+	},
+	btnTextStyle: {
+		color: 'white',
+	},
+});
 export default PlasmaHospital;
+
+// const styles = StyleSheet.creat({});
+
+// return (
+//     <SafeAreaView>
+//         <View></View>
+//     </SafeAreaView>
+// );
