3225d30049540a6df8a1798c17a378a4f5ab1b31
Nickinfinity <nikhilg.me@gmail.com>
2021-05-02 15:43:27 +0530

    delete expo status bar from app.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DonorDashBoardScreen from './src/screen/DonorDashBoardScreen';
